147fd5f90a53284b0741cc012d8de66e3ef8fc86
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-03-25 23:39:45 +0100

    Added WordService with method to get a Word from String, closes #77

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/WordService.java
@@ -0,0 +1,33 @@
+package ch.uzh.ifi.hase.soprafs24.service;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Word;
+import ch.uzh.ifi.hase.soprafs24.exceptions.WordNotFoundException;
+import ch.uzh.ifi.hase.soprafs24.repository.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class WordService {
+    private final WordRepository wordRepository;
+
+    @Autowired
+    public WordService(@Qualifier("wordRepository") WordRepository wordRepository) {
+        this.wordRepository = wordRepository;
+    }
+
+    public Word getWordFromString(String name) {
+        if (wordExists(name)) {
+            return wordRepository.findByName(name);
+        }
+        else {
+            throw new WordNotFoundException(name);
+        }
+    }
+
+    public boolean wordExists(String name) {
+        return wordRepository.findByName(name) != null;
+    }
+}
