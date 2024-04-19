bc01af856222cc445bfe0407ad019c5d20573630
Timon Leupp <timon@leupp.net>
2024-04-19 11:28:06 +0200

    First websocket experiment #103 #121

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LoveController.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class LoveController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    private final UserService userService;
+
+    LoveController(SimpMessagingTemplate messagingTemplate, UserService userService) {
+        this.messagingTemplate = messagingTemplate;
+        this.userService = userService;
+    }
+
+    @PostMapping("/greetings")
+    @ResponseStatus(HttpStatus.OK)
+    public void greet(@RequestHeader String userToken) {
+        User checkedUser = userService.checkToken(userToken);
+        if (checkedUser == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+
+        messagingTemplate.convertAndSend("/topic/greetings", checkedUser.getUsername() + " says I love you");
+        System.out.println("greeted");
+    }
+}
