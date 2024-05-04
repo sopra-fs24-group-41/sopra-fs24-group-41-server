fb349f8aad03abe91c59342bfde17dce61c8143e
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-05-04 13:53:00 +0200

    Added FiniteFusion game mode, closes #155

--- src/main/java/ch/uzh/ifi/hase/soprafs24/constant/GameMode.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/constant/GameMode.java
@@ -1,5 +1,5 @@
 package ch.uzh.ifi.hase.soprafs24.constant;
 
 public enum GameMode {
-    STANDARD, FUSIONFRENZY, WOMBOCOMBO
+    STANDARD, FUSIONFRENZY, WOMBOCOMBO, FINITEFUSION
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/game/FiniteFusionGame.java
@@ -0,0 +1,59 @@
+package ch.uzh.ifi.hase.soprafs24.game;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Combination;
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
+import ch.uzh.ifi.hase.soprafs24.entity.PlayerWord;
+import ch.uzh.ifi.hase.soprafs24.entity.Word;
+import ch.uzh.ifi.hase.soprafs24.service.CombinationService;
+import ch.uzh.ifi.hase.soprafs24.service.PlayerService;
+import ch.uzh.ifi.hase.soprafs24.service.WordService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+public class FiniteFusionGame extends Game {
+    private Integer startingUses = 10;
+
+    public FiniteFusionGame(PlayerService playerService, CombinationService combinationService, WordService wordService) {
+        super(playerService, combinationService, wordService);
+    }
+
+    public void setupPlayers(List<Player> players) {
+        setupStartingWords();
+        Word targetWord = wordService.getRandomWordWithinReachability(0.1, 0.3);
+        for (Player player : players) {
+            playerService.resetPlayer(player);
+            player.addWords(startingWords, startingUses);
+            player.setTargetWord(targetWord);
+        }
+    }
+
+    public Word makeCombination(Player player, List<Word> words) {
+        if (words.size() == 2) {
+            PlayerWord playerWord1 = player.getPlayerWord(words.get(0));
+            PlayerWord playerWord2 = player.getPlayerWord(words.get(1));
+            if (playerWord1.getUses() > 0 && playerWord2.getUses() > 0) {
+                playerWord1.addUses(-1);
+                playerWord2.addUses(-1);
+                Combination combination = combinationService.getCombination(words.get(0), words.get(1));
+                Word result = combination.getResult();
+                player.addWord(result, 1);
+                return result;
+            }
+            String errorMessage = "No more uses of ingredient words left!";
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+        }
+
+        String errorMessage = "Finite Fusion only allows combination of exactly two words!";
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+    }
+
+    public boolean winConditionReached(Player player) {
+        if (player.getWords().contains(player.getTargetWord())) {
+            player.addPoints(1000);
+            return true;
+        };
+        return false;
+    }
+}
