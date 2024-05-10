95b9164412488cf042f7c179f2b61f4b20a272f4
Timon Leupp <timon@leupp.net>
2024-05-10 13:18:34 +0200

    Changed the representation of the instruction enum to use lowercase #184

--- src/main/java/ch/uzh/ifi/hase/soprafs24/constant/Instruction.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/constant/Instruction.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.constant;
 
 public enum Instruction {
-    START, STOP, KICK, UPDATE
+    START, STOP, KICK, UPDATE;
+
+    public String toString() {
+        return this.name().toLowerCase();
+    }
 }
