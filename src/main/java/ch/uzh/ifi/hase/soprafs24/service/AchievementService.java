d7c2fdd5afcc769cb2adb72072dc6f301026e66a
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-05-14 16:47:26 +0200

    Added awardAchievements method to service with first example achievement, #175

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/AchievementService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/AchievementService.java
@@ -1,10 +1,16 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
+import ch.uzh.ifi.hase.soprafs24.entity.Achievement;
+import ch.uzh.ifi.hase.soprafs24.entity.Combination;
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.repository.AchievementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 
 @Service
@@ -15,5 +21,25 @@ public class AchievementService {
     @Autowired
     public AchievementService(@Qualifier("achievementRepository") AchievementRepository achievementRepository) {
         this.achievementRepository = achievementRepository;
+    }
+
+    public Achievement getAchievement(Achievement achievement) {
+        Achievement foundAchievement = achievementRepository.findByName(achievement.getName());
+        if (foundAchievement == null) {
+            return achievementRepository.saveAndFlush(achievement);
+        }
+        return foundAchievement;
+    }
+
+    public void awardAchievements(Player player, Combination combination) {
+        User user = player.getUser();
+        if (user == null) return;
+
+        if (combination.getResult().getName().equals("mud")) {
+            user.addAchievement(getAchievement(new Achievement(
+                    "This stuff is everywhere!",
+                    "Create mud through any combination",
+                    "MudPuddle.png")));
+        }
     }
 }
