99033d38a00e2637bfb4467787f4e5a86c7dad73
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-05-15 09:37:36 +0200

    Made set for achievements to iterate over, #175

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/AchievementService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/AchievementService.java
@@ -1,22 +1,28 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
-import ch.uzh.ifi.hase.soprafs24.entity.Achievement;
+import ch.uzh.ifi.hase.soprafs24.entity.achievements.Achievement;
 import ch.uzh.ifi.hase.soprafs24.entity.Combination;
 import ch.uzh.ifi.hase.soprafs24.entity.Player;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.entity.achievements.CreatedMud;
+import ch.uzh.ifi.hase.soprafs24.entity.achievements.CreatedZaddy;
 import ch.uzh.ifi.hase.soprafs24.repository.AchievementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Service
 @Transactional
 public class AchievementService {
     private final AchievementRepository achievementRepository;
+    private final Set<Achievement> achievements = new HashSet<Achievement>();
 
     @Autowired
     public AchievementService(@Qualifier("achievementRepository") AchievementRepository achievementRepository) {
@@ -28,15 +34,17 @@ public class AchievementService {
                 .orElse(achievementRepository.saveAndFlush(achievement));
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() {
+        achievements.addAll(Set.of(get(new CreatedMud()), get(new CreatedZaddy())));
+    }
+
     public void awardAchievements(Player player, Combination combination) {
         User user = player.getUser();
         if (user == null) return;
 
-        if (combination.getResult().getName().equals("mud")) {
-            user.addAchievement(getAchievement(new Achievement(
-                    "This stuff is everywhere!",
-                    "Create mud through any combination",
-                    "MudPuddle.png")));
+        for (Achievement achievement : achievements) {
+            achievement.unlock(player, combination);
         }
     }
 }
