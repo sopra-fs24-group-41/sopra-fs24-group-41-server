3ad61b3afc559b8e479246a10ba6e014b2be06cb
Alexandru Hurjui (Aquamarine-12) <hurjuialexandru12@gmail.com>
2024-05-19 10:59:49 +0200

    fix some warnings #133

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/CombinationServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/CombinationServiceTest.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 
 class CombinationServiceTest {
 
--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.internal.junit.MockitoTestListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
