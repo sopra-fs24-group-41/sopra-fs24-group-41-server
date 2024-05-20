8d9672837e837e3dedef8859344a633b524553dd
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-05-20 10:22:30 +0200

    adapted equals() and hashCode() overrides

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/achievements/Achievement.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/achievements/Achievement.java
@@ -44,8 +44,8 @@ public abstract class Achievement implements Serializable {
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
         Achievement that = (Achievement) o;
         return Objects.equals(getName(), that.getName()) &&
@@ -56,7 +56,7 @@ public abstract class Achievement implements Serializable {
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        return this instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 
     public String getName() {
