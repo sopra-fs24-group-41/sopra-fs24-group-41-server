716c40778feb1603bf4b61ab8b434235912979c1
na50r <shanrosan@gmail.com>
2024-05-22 14:51:20 +0200

    Removed Test for Prompt Tweaking

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/CombinationServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/CombinationServiceIntegrationTest.java
@@ -10,11 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebAppConfiguration
@@ -71,57 +66,6 @@ class CombinationServiceIntegrationTest {
 
             assertTrue(word.getReachability() >= minReachability);
             assertTrue(word.getReachability() <= maxReachability);
-        }
-    }
-
-    @Test
-    void makeCombinations_manyCombinations_testVertexAPI() {
-        ArrayList<Word> startingWords = new ArrayList<>(Arrays.asList(new Word("water"), new Word("earth"),
-                new Word("fire"), new Word("air")));
-
-        for (Word word : startingWords) {
-            Word foundWord = wordService.getWord(word);
-            foundWord.setDepth(0);
-            foundWord.setReachability(1e6);
-            wordService.saveWord(foundWord);
-        }
-
-        combinationService.makeCombinations(150);
-        int warningCount = 0;
-        int max = 0;
-        HashMap<String, Integer> wordCounts = new HashMap<>();
-
-        for (Combination combination : combinationRepository.findAll()) {
-            String resultWord = combination.getResult().getName();
-            if (resultWord.contains(combination.getWord1().getName()) || resultWord.contains(combination.getWord2().getName())) {
-                System.out.println("##### WARNING");
-                warningCount++;
-            }
-
-            if(resultWord.length()>max){
-                max = resultWord.length();
-            }
-
-            if(wordCounts.containsKey(resultWord)){
-                wordCounts.put(resultWord, wordCounts.get(resultWord)+1);
-            }
-
-            if(!wordCounts.containsKey(resultWord)){
-                wordCounts.put(resultWord, 1);
-            }
-
-            else
-                System.out.println("#####");
-            System.out.println("i1: " + combination.getWord1().getName());
-            System.out.println("i2: " + combination.getWord2().getName());
-            System.out.println("res: " + resultWord);
-        }
-        System.out.println("Number of Result Words that used Input Words: " + warningCount);
-        System.out.println("Max Number of Chars in Result Word: " + max);
-        System.out.println("Number of Unique Words: " + wordCounts.size());
-        System.out.println("Word Counts:");
-        for (String key : wordCounts.keySet()) {
-            System.out.println(key + ": " + wordCounts.get(key));
         }
     }
 }
